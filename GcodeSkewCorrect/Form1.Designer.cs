b9fc423a59236c592ec70ab625ab3c4c9cb28b88
Attila Csiki <csikiati@gmail.com>
2017-02-07 10:48:55 +0100

    original commit

---
+++ GcodeSkewCorrect/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace GcodeSkewCorrect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.buttonCorrect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbOrigGcode = new System.Windows.Forms.TextBox();
+            this.tbXY = new System.Windows.Forms.TextBox();
+            this.tbZX = new System.Windows.Forms.TextBox();
+            this.tbZY = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(12, 12);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(107, 23);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "Open Gcode";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // buttonCorrect
+            // 
+            this.buttonCorrect.Location = new System.Drawing.Point(12, 84);
+            this.buttonCorrect.Name = "buttonCorrect";
+            this.buttonCorrect.Size = new System.Drawing.Size(107, 23);
+            this.buttonCorrect.TabIndex = 1;
+            this.buttonCorrect.Text = "Skew correction";
+            this.buttonCorrect.UseVisualStyleBackColor = true;
+            this.buttonCorrect.Click += new System.EventHandler(this.buttonCorrect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "delta XY°";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(510, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "delta ZY°";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(285, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "delta ZX°";
+            // 
+            // tbOrigGcode
+            // 
+            this.tbOrigGcode.Location = new System.Drawing.Point(126, 14);
+            this.tbOrigGcode.Name = "tbOrigGcode";
+            this.tbOrigGcode.Size = new System.Drawing.Size(542, 20);
+            this.tbOrigGcode.TabIndex = 5;
+            // 
+            // tbXY
+            // 
+            this.tbXY.Location = new System.Drawing.Point(126, 49);
+            this.tbXY.Name = "tbXY";
+            this.tbXY.Size = new System.Drawing.Size(100, 20);
+            this.tbXY.TabIndex = 6;
+            this.tbXY.Text = "5";
+            // 
+            // tbZX
+            // 
+            this.tbZX.Location = new System.Drawing.Point(342, 49);
+            this.tbZX.Name = "tbZX";
+            this.tbZX.Size = new System.Drawing.Size(100, 20);
+            this.tbZX.TabIndex = 7;
+            this.tbZX.Text = "0";
+            // 
+            // tbZY
+            // 
+            this.tbZY.Location = new System.Drawing.Point(568, 49);
+            this.tbZY.Name = "tbZY";
+            this.tbZY.Size = new System.Drawing.Size(100, 20);
+            this.tbZY.TabIndex = 8;
+            this.tbZY.Text = "10";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "*.gcode";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 380);
+            this.Controls.Add(this.tbZY);
+            this.Controls.Add(this.tbZX);
+            this.Controls.Add(this.tbXY);
+            this.Controls.Add(this.tbOrigGcode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCorrect);
+            this.Controls.Add(this.buttonOpen);
+            this.Name = "Form1";
+            this.Text = "Gcode Skew Correction";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Button buttonCorrect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbOrigGcode;
+        private System.Windows.Forms.TextBox tbXY;
+        private System.Windows.Forms.TextBox tbZX;
+        private System.Windows.Forms.TextBox tbZY;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ GcodeSkewCorrect/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GcodeSkewCorrect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private double DegreeToRadian(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = @"E:\Autodesk\STL";
+            openFileDialog1.Title = "Browse Gcode files";
+
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            openFileDialog1.DefaultExt = "gcode";
+            openFileDialog1.Filter = "G-code (*.gcode)|*.gcode|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbOrigGcode.Text = openFileDialog1.FileName;
+            }
+
+        }
+
+        private void buttonCorrect_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            string line;
+            string newline;
+
+            double yMultiplier = Math.Round(Math.Tan((DegreeToRadian(Convert.ToDouble(tbXY.Text)))),3); //calculate the trigo stuff once to save precious process time
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file = new System.IO.StreamReader(tbOrigGcode.Text);
+            System.IO.StreamWriter file2 = new System.IO.StreamWriter("D:\\SkewCorrectedOutput.gcode");
+
+            file2.WriteLine("; Original unconverted filename:");
+            file2.WriteLine("; "+ tbOrigGcode.Text);
+            file2.WriteLine("; XY°=" + tbXY.Text);
+            file2.WriteLine("; ZX°=" + tbZX.Text);
+            file2.WriteLine("; ZY°=" + tbZY.Text);
+            file2.WriteLine("; Converted by GcodeSkewCorrect (c) Attila Csiki 2017");
+
+            while ((line = file.ReadLine()) != null) //do it until end of file
+            {
+                newline = line;
+                var splitted = line.Split(' '); //splitting the line into words
+                if (splitted[0].StartsWith("G1")) // only G1 commands are important so if first word is G1 then
+                {
+                    //initializing line level variables
+                    //double newX = 0;
+                    double newY = 0;
+                    var oldX = "";
+                    var oldY = "";
+                    var oldF = "";
+                    var oldE = "";
+                    char c = new char();
+                    bool isF = false;
+                    bool isX = false;
+                    bool isY = false;
+                    bool isE = false;
+
+                    isF = false;
+                    isX = false;
+                    isY = false;
+                    isE = false;
+
+                    for (int i = 1; i < splitted.Count(); i++) //going through all relevant values
+                    {
+
+
+                        if (splitted[i].Length > 1)
+                        {
+                            c = splitted[i][0];
+                            switch (c) //getting all the F, X, Y, and E values from the G1 comand
+                            {
+                                case 'F':
+                                    oldF = splitted[i].TrimStart('F');
+                                    isF = true;
+                                    break;
+                                case 'X':
+                                    oldX = splitted[i].TrimStart('X');
+                                    isX = true;
+                                    break;
+                                case 'Y':
+                                    oldY = splitted[i].TrimStart('Y');
+                                    isY = true;
+                                    break;
+                                case 'E':
+                                    oldE = splitted[i].TrimStart('E');
+                                    isE = true;
+                                    break;
+                                default:
+                                    break;
+                            }
+
+
+                        }
+                    }
+
+                    if (isX & isY & isE)
+                    {
+                        double a = double.Parse(oldY, System.Globalization.CultureInfo.InvariantCulture);
+                        double b = double.Parse(oldX, System.Globalization.CultureInfo.InvariantCulture);
+                        double m = yMultiplier;
+                        newY = a - (b * m);
+
+                        if (isF)
+                        {
+                            newline = "G1 " + "F" + oldF + " " + "X" + oldX + " " + "Y" + Math.Round(newY, 3).ToString().Replace(',','.') + " " + "E" + oldE;
+                        }
+                        else
+                        {
+                            newline = "G1 " + "X" + oldX + " " + "Y" + Math.Round(newY, 3).ToString().Replace(',', '.') + " " + "E" + oldE;
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    newline = line;
+                }
+
+                file2.WriteLine(newline);
+                //Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            file2.Close();
+
+            MessageBox.Show("File is converted. Rejoice in teh Lord!", "Breaking news");
+
+            // Suspend the screen.
+            Console.ReadLine();
+        }
+    }
+}
